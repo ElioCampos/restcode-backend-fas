7dfeca1acceb4f82679bb34c0fe16ddeeccc70da
vincizLe <u201822641@upc.edu.pe>
2021-06-21 17:28:24 -0500

    Add BDD Testing

---
+++ InventoryTest/Features/AddProductToCategory.feature
@@ -0,0 +1,10 @@
+﻿Feature: AddProductToCategory
+	As owner I want to add a new product for my restaurant in a specific category
+
+@mytag
+Scenario: Add new product to category
+	Given the owner wants to add on product endpoint 
+	When owner add a new product
+	| Name             | Price |
+	| Pollo a la brasa |  20   |
+	Then the product will be added succesfully
---
+++ InventoryTest/Steps/AddProductToCategorySteps.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using RestCode_WebApplication.Domain.Models;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace InventoryTest.Steps
+{
+    [Binding]
+    public class AddProductToCategorySteps
+    {
+        public static RestClient restClient;
+        public static RestRequest restRequest;
+        public static IRestResponse response;
+        private static Product product;
+
+        [Given(@"the owner wants to add on product endpoint")]
+        public void GivenTheOwnerWantsToAddOnProductEndpoint()
+        {
+            restClient = new RestClient("https://localhost:44316/");
+            restRequest = new RestRequest("api/products", Method.POST);
+            restRequest.RequestFormat = DataFormat.Json;
+        }
+        
+        [When(@"owner add a new product")]
+        public void WhenOwnerAddANewProduct(Table table)
+        {
+            product = table.CreateInstance<Product>();
+            product = new Product()
+            {
+                Name = "Pollo a la brasa",
+                Price = 20
+            };
+            restRequest.AddJsonBody(product);
+            response = restClient.Execute(restRequest);
+        }
+        
+        [Then(@"the product will be added succesfully")]
+        public void ThenTheProductWillBeAddedSuccesfully()
+        {
+            Assert.That("Pollo a la brasa", Is.EqualTo(product.Name));
+            Assert.That(20, Is.EqualTo(product.Price));
+        }
+    }
+}
